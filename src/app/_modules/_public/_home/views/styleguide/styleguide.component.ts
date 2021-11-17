2deade1fb77eb41d6abb8406177ea0923b9a5a54
Steven Jonscher <sj@braune-digital.com>
2021-11-17 16:18:30 +0100

    Add mocked data to ranking table on styleguide

--- src/app/_modules/_public/_home/views/styleguide/styleguide.component.ts
+++ src/app/_modules/_public/_home/views/styleguide/styleguide.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   ViewEncapsulation,
 } from '@angular/core';
+import { ScorerEntry } from '../../../../../_models';
 
 @Component({
   selector: 'app-styleguide',
@@ -10,4 +11,37 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class StyleguideComponent {}
+export class StyleguideComponent {
+  playerRankingMock: ScorerEntry[] = [
+    {
+      last_name: 'Last',
+      first_name: 'test',
+      penalty_2: 2,
+      assists: 1,
+      penalty_10: 10,
+      position: 1,
+      player_id: 1,
+      games: 3,
+      goals: 3,
+      penalty_2and2: 2,
+      sort: 1,
+      penalty_5: 5,
+      penalty_ms: 2,
+    },
+    {
+      last_name: 'Last',
+      first_name: 'test',
+      penalty_2: 2,
+      assists: 1,
+      penalty_10: 10,
+      position: 1,
+      player_id: 1,
+      games: 3,
+      goals: 3,
+      penalty_2and2: 2,
+      sort: 1,
+      penalty_5: 5,
+      penalty_ms: 2,
+    },
+  ];
+}
