9c2b6a435b6f5eb25cd7c2adac40f2ae7224d728
Steven Jonscher <sj@braune-digital.com>
2021-11-18 10:02:21 +0100

    Add styles for ranking-table

--- src/app/_modules/_public/_home/views/styleguide/styleguide.component.ts
+++ src/app/_modules/_public/_home/views/styleguide/styleguide.component.ts
@@ -43,5 +43,20 @@ export class StyleguideComponent {
       penalty_5: 5,
       penalty_ms: 2,
     },
+    {
+      last_name: 'Last',
+      first_name: 'test',
+      penalty_2: 2,
+      assists: 1,
+      penalty_10: 10,
+      position: 1,
+      player_id: 1,
+      games: 3,
+      goals: 3,
+      penalty_2and2: 2,
+      sort: 1,
+      penalty_5: 5,
+      penalty_ms: 2,
+    },
   ];
 }
