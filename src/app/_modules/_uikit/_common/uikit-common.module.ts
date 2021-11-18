c339de222df4ef9095dcc22a516205e090038691
Manuel Schulte <ms@braune-digital.com>
2021-11-18 12:39:26 +0100

    [FE-26] feat: Add logo component

--- src/app/_modules/_uikit/_common/uikit-common.module.ts
+++ src/app/_modules/_uikit/_common/uikit-common.module.ts
@@ -1,7 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import * as Atoms from './components/atoms';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    Atoms.LogoComponent,
+  ],
+  exports: [
+    Atoms.LogoComponent,
+  ],
 })
 export class UikitCommonModule {}
