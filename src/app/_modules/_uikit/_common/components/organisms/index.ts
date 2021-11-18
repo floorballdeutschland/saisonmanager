6245cccc0cf6311bd45087b7085a09d37cdb24d9
Manuel Schulte <ms@braune-digital.com>
2021-11-18 12:44:04 +0100

    [FE-26] feat: Add league navigation component

---
+++ src/app/_modules/_uikit/_common/components/organisms/index.ts
@@ -0,0 +1,1 @@
+export * from './league-navigation/league-navigation.component';
--- src/app/_modules/_uikit/_common/uikit-common.module.ts
+++ src/app/_modules/_uikit/_common/uikit-common.module.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import * as Atoms from './components/atoms';
+import * as Organisms from './components/organisms';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
     Atoms.LogoComponent,
+    Organisms.LeagueNavigationComponent
   ],
   exports: [
     Atoms.LogoComponent,
+    Organisms.LeagueNavigationComponent
   ],
 })
 export class UikitCommonModule {}
