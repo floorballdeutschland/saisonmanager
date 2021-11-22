f3f027ac0f4c47d1e0a0fce943477787a7080cc0
Steven Jonscher <sj@braune-digital.com>
2021-11-22 14:30:45 +0100

    Add navigation to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UikitCommonModule } from '@floorball/uikit/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    UikitCommonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
