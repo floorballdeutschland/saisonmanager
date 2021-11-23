5918821fd25e851fd1892734f7de4893e1303546
Felix Baltruschat <fb@braune-digital.com>
2021-11-23 10:15:29 +0100

    unsubscribe on destroy

--- src/app/_modules/_public/_association/_host/_views/league-host/league-host.component.ts
+++ src/app/_modules/_public/_association/_host/_views/league-host/league-host.component.ts
@@ -1,29 +1,36 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LeagueService } from '@floorball/core';
 import { League } from '@floorball/types';
-import { Observable, tap } from 'rxjs';
+import { Observable, Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   templateUrl: './league-host.component.html',
   encapsulation: ViewEncapsulation.None,
 })
-export class LeagueHostComponent implements OnInit {
+export class LeagueHostComponent implements OnInit, OnDestroy {
   selectedLeague$!: Observable<League | null>;
+
+  private _destroy$ = new Subject<boolean>();
 
   constructor(
     private _leagueService: LeagueService,
     private _route: ActivatedRoute
   ) {}
 
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+
   ngOnInit(): void {
     this.selectedLeague$ = this._leagueService.selectedLeague$;
 
     this._route.params
       .pipe(
-        tap((_params) => {
+        tap(() => {
           this._leagueService.selectLeague(this._route);
-        })
+        }),
+        takeUntil(this._destroy$)
       )
       .subscribe();
   }
