6a82416669ceeef96f970a4e3aea456fa8e0f3f5
Steven Jonscher <sj@braune-digital.com>
2021-11-23 15:55:54 +0100

    Add team rankings with data to ranking view

--- src/app/_modules/_public/_association/_ranking/_views/ranking/ranking.component.ts
+++ src/app/_modules/_public/_association/_ranking/_views/ranking/ranking.component.ts
@@ -3,11 +3,48 @@ import {
   OnInit,
   ViewEncapsulation,
   ChangeDetectionStrategy,
+  OnDestroy,
+  ChangeDetectorRef,
 } from '@angular/core';
+import { Observable, Subject, takeUntil, tap } from 'rxjs';
+import { TableEntry } from '@floorball/types';
+import { LeagueService } from '@floorball/core';
 
 @Component({
   templateUrl: './ranking.component.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RankingComponent {}
+export class RankingComponent implements OnInit, OnDestroy {
+  teamRankings$?: Observable<TableEntry[] | null>;
+
+  private _destroy$ = new Subject<boolean>();
+
+  constructor(
+    private _leagueService: LeagueService,
+    private _cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this._leagueService.selectedLeague$
+      .pipe(
+        tap((league) => {
+          if (league?.id) {
+            this.getTeamRanking(league.id);
+            this._cdr.markForCheck();
+          }
+        }),
+        takeUntil(this._destroy$)
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.next(true);
+    this._destroy$.complete();
+  }
+
+  getTeamRanking(leagueNumber: number) {
+    this.teamRankings$ = this._leagueService.getTable(leagueNumber);
+  }
+}
--- src/app/_modules/_public/_association/_ranking/public-association-ranking.module.ts
+++ src/app/_modules/_public/_association/_ranking/public-association-ranking.module.ts
@@ -3,8 +3,13 @@ import { CommonModule } from '@angular/common';
 import { PublicAssociationRankingRoutingModule } from './public-association-ranking-routing.module';
 
 import * as Views from './_views';
+import { UikitTeamModule } from '@floorball/uikit/team';
 @NgModule({
   declarations: [Views.RankingComponent],
-  imports: [CommonModule, PublicAssociationRankingRoutingModule],
+  imports: [
+    CommonModule,
+    PublicAssociationRankingRoutingModule,
+    UikitTeamModule,
+  ],
 })
 export class PublicAssociationRankingModule {}
