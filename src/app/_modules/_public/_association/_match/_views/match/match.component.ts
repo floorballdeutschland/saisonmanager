21ba9616de158ca5f262866f6b1804dd6bf28619
Steven Jonscher <sj@braune-digital.com>
2021-11-24 16:25:35 +0100

    Get data for match view

--- src/app/_modules/_public/_association/_match/_views/match/match.component.ts
+++ src/app/_modules/_public/_association/_match/_views/match/match.component.ts
@@ -2,11 +2,34 @@ import {
   Component,
   ViewEncapsulation,
   ChangeDetectionStrategy,
+  OnInit,
 } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Game } from '@floorball/types';
+import { GameService } from '@floorball/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   templateUrl: './match.component.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MatchComponent {}
+export class MatchComponent implements OnInit {
+  match$?: Observable<Game | null>;
+
+  constructor(
+    private _gameService: GameService,
+    private _route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const matchId = this._route.snapshot?.paramMap.get('matchId');
+    if (matchId) {
+      this.getMatch(matchId);
+    }
+  }
+
+  getMatch(id: string) {
+    this.match$ = this._gameService.getGame(parseInt(id));
+  }
+}
