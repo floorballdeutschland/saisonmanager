fdb842d9dbf2cec63cea121b99beba446ff60370
Steven Jonscher <sj@braune-digital.com>
2021-11-25 10:07:18 +0100

    Add header, match history and lineup to match view

--- src/app/_modules/_public/_association/_match/public-association-match.module.ts
+++ src/app/_modules/_public/_association/_match/public-association-match.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 import { PublicAssociationMatchRoutingModule } from './public-association-match-routing.module';
 
 import * as Views from './_views';
+import { UikitCommonModule } from '@floorball/uikit/common';
+import { UikitMatchesModule } from '@floorball/uikit/matches';
+import { UikitTeamModule } from '@floorball/uikit/team';
 
 @NgModule({
   declarations: [Views.MatchComponent],
-  imports: [CommonModule, PublicAssociationMatchRoutingModule],
+  imports: [
+    CommonModule,
+    PublicAssociationMatchRoutingModule,
+    UikitCommonModule,
+    UikitMatchesModule,
+    UikitTeamModule,
+  ],
 })
 export class PublicAssociationMatchModule {}
