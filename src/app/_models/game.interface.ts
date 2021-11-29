f7892b8c174423a73ea6c8ede065f856bb2cd2f3
Jan Hoffmann <jho@jhoit.de>
2021-11-30 00:26:03 +0100

    add league, game_operation and date to game (see #5)

--- src/app/_models/game.interface.ts
+++ src/app/_models/game.interface.ts
@@ -28,13 +28,32 @@ export interface GamePlayers {
     guest: GamePlayerEntry[]
 }
 
+export interface GameResult {
+  home_goals: number
+  guest_goals: number
+  home_goals_period: number[]
+  guest_goals_period: number[]
+  overtime: boolean
+}
+
 export interface Game {
     id: number
     game_number: string
     start_time: string
+    date: Date
     audience: number
     home_team_name: string
     guest_team_name: string
     events: GameEvent[]
     players: GamePlayers
+    started: boolean
+    ended: boolean
+    result_string: string
+    result: GameResult
+    league_id: number
+    league_name: string
+    league_short_name: string
+    game_operation_id: number
+    game_operation_name: string
+    game_operation_short_name: string
 }
