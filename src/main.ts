54c0ec7f0e7754f82b3f8c7ee3bacf8ea64e0be9
Jan Hoffmann <jho@jhoit.de>
2021-12-03 18:36:46 +0100

    add sentry to main.ts (see #14)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import * as Sentry from '@sentry/angular'
+import { Integrations } from '@sentry/tracing'
+
+import { AppModule } from './app/app.module'
+import { environment } from './environments/environment'
+
+Sentry.init({
+  dsn: "https://68070f7ec5fe4ea0920b43c46893c759@o1082542.ingest.sentry.io/6092803",
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: ["localhost", "https://yourserver.io/api"],
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+})
 
 if (environment.production) {
-  enableProdMode();
+  enableProdMode()
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  .catch(err => console.error(err))
