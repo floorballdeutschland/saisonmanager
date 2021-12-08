d35a72c480fdbe07ce9425fb553532c4751df55e
Steven Jonscher <sj@braune-digital.com>
2021-12-08 10:21:14 +0100

    Fix component loading on route change on match view

--- src/app/_modules/_public/_association/_match/_views/match/match.component.ts
+++ src/app/_modules/_public/_association/_match/_views/match/match.component.ts
@@ -4,11 +4,12 @@ import {
   ChangeDetectionStrategy,
   OnInit,
   OnDestroy,
+  ChangeDetectorRef,
 } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Game } from '@floorball/types';
 import { AssociationService, GameService } from '@floorball/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './match.component.html',
@@ -21,8 +22,17 @@ export class MatchComponent implements OnInit, OnDestroy {
   constructor(
     private _associationService: AssociationService,
     private _gameService: GameService,
-    private _route: ActivatedRoute
-  ) {}
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _cdr: ChangeDetectorRef
+  ) {
+    _router.events.subscribe(() => {
+      const matchId = this._route.snapshot?.paramMap.get('matchId');
+      if (matchId) {
+        this.getMatch(matchId);
+      }
+    });
+  }
 
   ngOnDestroy(): void {
     this._associationService.displayAssociationHeader$.next(true);
@@ -38,5 +48,6 @@ export class MatchComponent implements OnInit, OnDestroy {
 
   getMatch(id: string) {
     this.match$ = this._gameService.getGame(parseInt(id));
+    this._cdr.markForCheck();
   }
 }
