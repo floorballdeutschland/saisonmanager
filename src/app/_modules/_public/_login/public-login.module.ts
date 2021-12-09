072483fb29f6bad7d79682771e1d376cac666d56
Steven Jonscher <sj@braune-digital.com>
2021-12-09 10:24:54 +0100

    Add form controls to form

--- src/app/_modules/_public/_login/public-login.module.ts
+++ src/app/_modules/_public/_login/public-login.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import * as Views from './views';
 import { PublicLoginRoutingModule } from './public-login-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [Views.LoginComponent],
-  imports: [CommonModule, PublicLoginRoutingModule],
+  imports: [CommonModule, PublicLoginRoutingModule, ReactiveFormsModule],
 })
 export class PublicLoginModule {}
--- src/app/_modules/_public/_login/views/login/login.component.ts
+++ src/app/_modules/_public/_login/views/login/login.component.ts
@@ -1,7 +1,31 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ControlsOf, FormControl, FormGroup } from '@ngneat/reactive-forms';
+import { Validators } from '@angular/forms';
+
+export interface LoginFormValue {
+  username: string;
+  password: string;
+}
 
 @Component({
   templateUrl: './login.component.html',
   encapsulation: ViewEncapsulation.None,
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup<ControlsOf<LoginFormValue>>;
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  loginSubmit(data: LoginFormValue) {
+    if (!this.loginForm.valid) {
+      return;
+    }
+
+    console.log(data);
+  }
+}
