9aa120508385a62a3784f41e260ce7ae32a674e3
Steven Jonscher <sj@braune-digital.com>
2021-12-10 11:01:22 +0100

    Get data on all-matches

--- src/app/_modules/_public/_association/_all-matches-overview/_views/all-matches/all-matches.component.ts
+++ src/app/_modules/_public/_association/_all-matches-overview/_views/all-matches/all-matches.component.ts
@@ -1,8 +1,13 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  OnDestroy,
+  OnInit,
   ViewEncapsulation,
 } from '@angular/core';
+import { Observable, Subject, takeUntil, tap } from 'rxjs';
+import { GameScheduleEntry } from '@floorball/types';
+import { LeagueService } from '@floorball/core';
 
 @Component({
   selector: 'fb-all-matches',
@@ -10,4 +15,32 @@ import {
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AllMatchesComponent {}
+export class AllMatchesComponent implements OnInit, OnDestroy {
+  matches$?: Observable<GameScheduleEntry[] | null>;
+
+  private _destroy$ = new Subject<boolean>();
+
+  constructor(private _leagueService: LeagueService) {}
+
+  ngOnInit() {
+    this._leagueService.selectedLeague$
+      .pipe(
+        tap((league) => {
+          if (league?.id) {
+            this.getMatches(league.id);
+          }
+        }),
+        takeUntil(this._destroy$)
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.next(true);
+    this._destroy$.complete();
+  }
+
+  getMatches(leagueNumber: number) {
+    this.matches$ = this._leagueService.getGameSchedule(leagueNumber);
+  }
+}
