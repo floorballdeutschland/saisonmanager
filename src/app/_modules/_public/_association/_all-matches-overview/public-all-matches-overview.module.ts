f33a4e4abb6dc3e8401e2badbe60718e435f9cde
Steven Jonscher <sj@braune-digital.com>
2021-12-10 11:34:46 +0100

    WIP Add all match-encounters to all-matches

--- src/app/_modules/_public/_association/_all-matches-overview/public-all-matches-overview.module.ts
+++ src/app/_modules/_public/_association/_all-matches-overview/public-all-matches-overview.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as Views from './_views';
 import { PublicAllMatchesOverviewRoutingModule } from './public-all-matches-overview-routing.module';
+import { UikitMatchesModule } from '@floorball/uikit/matches';
 
 @NgModule({
   declarations: [Views.AllMatchesComponent],
-  imports: [CommonModule, PublicAllMatchesOverviewRoutingModule],
+  imports: [
+    CommonModule,
+    PublicAllMatchesOverviewRoutingModule,
+    UikitMatchesModule,
+  ],
 })
 export class PublicAllMatchesOverviewModule {}
