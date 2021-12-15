f58fa3080a76804326335ed14562fe27e8b4a901
Steven Jonscher <sj@braune-digital.com>
2021-12-15 11:57:58 +0100

    Fix closing on sidebar header

--- src/app/_modules/_uikit/_common/components/organisms/sidebar/sidebar.component.ts
+++ src/app/_modules/_uikit/_common/components/organisms/sidebar/sidebar.component.ts
@@ -1,15 +1,14 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   ComponentRef,
-  ElementRef,
   Input,
-  ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
 import { OverlayService } from '@floorball/core';
 import { GameOperation } from '@floorball/types';
-import { Subject } from 'rxjs';
+import { take, tap } from 'rxjs';
 import { MobileHeaderComponent } from '..';
 
 @Component({
@@ -21,22 +20,34 @@ import { MobileHeaderComponent } from '..';
 export class SidebarComponent {
   @Input()
   association?: GameOperation | null;
-  overlayComponentRef?: ComponentRef<MobileHeaderComponent>;
 
+  overlayComponentRef?: ComponentRef<MobileHeaderComponent>;
   menuIsOpen = false;
 
-  constructor(private _overlayService: OverlayService) {}
+  constructor(
+    private _overlayService: OverlayService,
+    private _cdr: ChangeDetectorRef
+  ) {}
 
   openMenu() {
     this.overlayComponentRef = this._overlayService.showScrollBlockingOverlay(
       MobileHeaderComponent
     );
+
     this.menuIsOpen = true;
+
+    this.overlayComponentRef?.instance.onClose$
+      .pipe(
+        tap(() => {
+          this.menuIsOpen = false;
+          this._cdr.markForCheck();
+        }),
+        take(1)
+      )
+      .subscribe();
   }
 
   closeMenu() {
     this.overlayComponentRef?.instance.onClose$.next(true);
-
-    this.menuIsOpen = false;
   }
 }
