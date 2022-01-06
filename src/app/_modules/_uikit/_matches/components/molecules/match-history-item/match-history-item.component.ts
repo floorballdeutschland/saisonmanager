8a961eeace0cc68258f2f0fd3583eb2923201d35
Felix Baltruschat <fb@braune-digital.com>
2022-01-06 15:16:21 +0100

    fix some issues

--- src/app/_modules/_uikit/_matches/components/molecules/match-history-item/match-history-item.component.ts
+++ src/app/_modules/_uikit/_matches/components/molecules/match-history-item/match-history-item.component.ts
@@ -13,6 +13,14 @@ import { Game, GameEvent } from '@floorball/types';
   encapsulation: ViewEncapsulation.None,
 })
 export class MatchHistoryItemComponent {
+  /**
+   * TODO
+   * Fehlende EventTypes
+   * penalty_shots
+   * penalty_shot
+   * http://localhost:4200/fvd/1044/spiel/17098
+   */
+
   @Input()
   match!: Game;
 
