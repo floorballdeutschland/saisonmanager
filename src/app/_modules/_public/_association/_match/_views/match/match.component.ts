ce2df5f1ff999294398b9d44a8e797a6fe3830d7
Pascal Riedel <pascal.riedel@gmail.com>
2022-09-09 18:05:51 +0200

    fix tab selection

--- src/app/_modules/_public/_association/_match/_views/match/match.component.ts
+++ src/app/_modules/_public/_association/_match/_views/match/match.component.ts
@@ -27,7 +27,7 @@ export class MatchComponent implements OnInit, OnDestroy {
   selectedAssociation$!: Observable<GameOperation | null>;
 
   public isLoggedIn$ = this._sessionService.isLoggedIn$;
-  public tab = 'secretary';
+  public tab = 'public';
   public addDialogOpen = '';
   public squadHistoryDialogOpen = '';
 
