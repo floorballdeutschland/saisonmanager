b089f5678ed7acc4d1a124728bdfc958d7bb1fc3
Pascal Riedel <pascal.riedel@gmail.com>
2022-09-14 20:45:45 +0200

    added owngoal and NA player to events, optimised events form, see #70

--- src/app/_modules/_uikit/_matches/components/organisms/match-event-form/match-event-form.component.ts
+++ src/app/_modules/_uikit/_matches/components/organisms/match-event-form/match-event-form.component.ts
@@ -69,10 +69,15 @@ export class MatchEventFormComponent implements OnInit {
   startTime = '';
   minutes?: number;
   seconds?: number;
-  playerId?: number;
-  assistPlayerId?: number;
-  penalty?: number;
-  penaltyCode?: number;
+
+  playerSearchNumber?: number;
+  playerNumber = 0;
+  assistError = false;
+  assistPlayerSearchNumber?: number;
+  assistPlayerNumber = 0;
+  playerError = false;
+  penalty = 0;
+  penaltyCode = 0;
   with_ps?: boolean;
 
   visitors?: number;
@@ -208,6 +213,36 @@ export class MatchEventFormComponent implements OnInit {
     return s;
   }
 
+  public searchPlayerByNumber(side: string, number: number, isAssist: boolean) {
+    const tmpSide = side === 'home' ? 'home' : 'guest';
+    const player = this.match.players[tmpSide]?.find(
+      (p) => p.trikot_number === number
+    );
+
+    if (isAssist) {
+      this.assistError = number !== 0 && !player;
+    } else {
+      this.playerError = number !== 0 && !player;
+    }
+
+    if (isAssist) {
+      this.assistPlayerNumber = player?.trikot_number || 0;
+    } else {
+      this.playerNumber = player?.trikot_number || 0;
+    }
+  }
+
+  public submitDisabled(): boolean {
+    return (
+      (!this.editLive && this.startTime === '') ||
+      (['penalty'].includes(this.type) &&
+        (!this.penaltyCode || !this.penalty)) ||
+      (['goal', 'penalty'].includes(this.type) && !this.playerNumber) ||
+      (['goal', 'penalty', 'timeout'].includes(this.type) &&
+        (!this.minutes || !this.seconds))
+    );
+  }
+
   public submitEvent() {
     const time = this.minutes + ':' + this.pad(this.seconds || 0, 2);
 
@@ -220,19 +255,6 @@ export class MatchEventFormComponent implements OnInit {
       this.type === 'goal' && this.team === 'guest'
         ? (this.match.result?.guest_goals || 0) + 1
         : this.match.result?.guest_goals || 0;
-
-    let player;
-    if (this.team === 'home') {
-      player =
-        this.match.players.home?.find(
-          (p: { player_id: number | undefined }) => p.player_id == this.playerId
-        )?.trikot_number || 0;
-    } else {
-      player =
-        this.match.players.guest?.find(
-          (p: { player_id: number | undefined }) => p.player_id == this.playerId
-        )?.trikot_number || 0;
-    }
 
     switch (this.type) {
       case 'next':
@@ -269,21 +291,7 @@ export class MatchEventFormComponent implements OnInit {
         break;
       case 'goal':
         if (this.currentPeriod) {
-          let assist;
-          if (this.team === 'home') {
-            assist =
-              this.match.players.home?.find(
-                (p: { player_id: number | undefined }) =>
-                  p.player_id == this.assistPlayerId
-              )?.trikot_number || 0;
-          } else {
-            assist =
-              this.match.players.guest?.find(
-                (p: { player_id: number | undefined }) =>
-                  p.player_id == this.assistPlayerId
-              )?.trikot_number || 0;
-          }
-
+          // fix typeerror with tostring and parseint
           // eslint-disable-next-line prefer-const
           let goal: {
             home_number?: number;
@@ -293,8 +301,17 @@ export class MatchEventFormComponent implements OnInit {
             penalty_code_id?: number;
           } =
             this.team === 'home'
-              ? { home_number: player, home_assist: assist }
-              : { guest_number: player, guest_assist: assist };
+              ? {
+                  home_number: parseInt(this.playerNumber.toString(), 10),
+                  home_assist: parseInt(this.assistPlayerNumber.toString(), 10),
+                }
+              : {
+                  guest_number: parseInt(this.playerNumber.toString(), 10),
+                  guest_assist: parseInt(
+                    this.assistPlayerNumber.toString(),
+                    10
+                  ),
+                };
 
           if (this.with_ps) {
             goal.penalty_code_id = 23;
@@ -329,7 +346,10 @@ export class MatchEventFormComponent implements OnInit {
               period: parseInt(this.currentPeriod, 10),
               home_goals,
               guest_goals,
-              [this.team === 'home' ? 'home_number' : 'guest_number']: player,
+              [this.team === 'home' ? 'home_number' : 'guest_number']: parseInt(
+                this.playerNumber.toString(),
+                10
+              ),
               penalty_id: this.penalty,
               penalty_code_id: this.penaltyCode,
             })
