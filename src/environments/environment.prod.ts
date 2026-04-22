b385830b09ff9116384343bae39b7013d082c384
Daniel Kehne <daniel.kehne@web.de>
2026-04-22 15:07:24 +0200

    config: Frontend-API-Key für Produktion setzen

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiURL: 'https://saisonmanager.org/api/v2/',
-  frontendApiKey: '',
+  frontendApiKey:
+    '2d56940cf230fde8a0a25ceacaeb1f7f43917d82179a34af324d03f7eacdf055',
 };
